a58235be265d4bcdb084a62fd0e66a56a33a0ec9
Alex S <insten490@gmail.com>
2024-01-26 22:33:53 -0600

    Re-arranged code folders to create a market package

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+from market import app
+from market.models import Item
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.app_context().push()
-db = SQLAlchemy(app)
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
+
+if __name__ == '__main__':
+    app.run(debug=True)
