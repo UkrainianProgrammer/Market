5db9dd9dd6f5c9c0a75bc9d65ede69b2491a44d6
Alex S <insten490@gmail.com>
2024-01-29 15:39:51 -0600

    Added registration form and labels

--- market/__init__.py
+++ market/__init__.py
@@ -3,5 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'e6d9bf80fc62756603601c62'
 app.app_context().push()
 db = SQLAlchemy(app)
