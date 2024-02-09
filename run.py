6b15c78c86da70a4207e55a92df6bc1fac7423a2
Alex S <insten490@gmail.com>
2024-02-09 13:34:50 -0600

    Put together a login page

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash
 from market import app
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
 
 @app.route('/')
@@ -32,7 +32,8 @@ def register_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
