bc49ba1d5d10e6d61973b690dae612a4dd4873e1
Alex S <insten490@gmail.com>
2024-02-14 16:34:40 -0600

    Continue to implement login logic

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'e6d9bf80fc62756603601c62'
 app.app_context().push()
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 db = SQLAlchemy(app)
